57ffadca91e2686bc6fbc681ae0b247415488898
erenakpinar <erenakpinar123@gmail.com>
2016-01-15 16:41:55 +0200

    AspNetMVCSessionKullanimi
    Asp.Net MVC Session Kullanımı Örnek Proje

---
+++ AspNetMVCSessionKullanimi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVCSessionKullanimi.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Session["PageName"] = "Home";
+            return View();
+        }
+
+    }
+}
